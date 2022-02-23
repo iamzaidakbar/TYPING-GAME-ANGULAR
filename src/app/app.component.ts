1f421dbe4e9cbf825beb03c63dae56816aa20e36
iamzaidakbar <iamzaidakbar@gmail.com>
2022-02-23 12:14:13 +0530

    Changes Made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing-challenge';
+
+  randomText = lorem.sentence();
+  userText = '';
+
+  onkeyUp(value: string) {
+    this.userText = value;
+  }
+
+  compare(letter: string, userText: string) {
+    if (!userText) {
+      return 'pending';
+    }
+    return letter === userText ? 'correct' : 'incorrect';
+  }
 }
